40cd897c28f8ffe344f5fac2419a1cb41aa711b2
matloob-altaf <21100164@lums.edu.pk>
2020-09-01 03:52:34 +0000

    Added popular datasets on home page

--- ckanext/ckan_theme/plugin.py
+++ ckanext/ckan_theme/plugin.py
@@ -1,13 +1,56 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+def most_popular_datasets():
+    '''
+    A function to return top 4 datasets based on popularity
+
+    '''
+    import requests
+    import json
+    import socket 
+    
+    # Get the ip address   
+    hostname = socket.gethostname()    
+    ip_addr = socket.gethostbyname(hostname)
+    
+    # Make the HTTP request.
+    url = 'http://' + str(ip_addr) + '/api/3/action/package_search?sort=popularity+desc&rows=4'
+    response = requests.get(url)
+
+    assert response.status_code == 200
+
+    # Use the json module to load CKAN's response into a dictionary.
+    response_dict = response.json()
+    
+    # Check the contents of the response.
+    assert response_dict['success'] is True
+    results = response_dict['result']['results']
+    # output = []
+    # for result in results:
+    #     output.append(result['name'])
+            
+    # return output
+    return results
 
 class Ckan_ThemePlugin(plugins.SingletonPlugin):
+    # IConfigurer
     plugins.implements(plugins.IConfigurer)
 
-    # IConfigurer
+    # Declare that this plugin will implement ITemplateHelpers.
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ckan_theme')
+    
+    def get_helpers(self):
+        '''Register the most_popular_datasets() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'ckan_theme_most_popular_datasets': most_popular_datasets}
