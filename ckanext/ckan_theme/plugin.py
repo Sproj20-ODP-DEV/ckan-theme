62a3604236bcbb7e03c4508a2ac88b6dba386395
matloob-altaf <21100164@lums.edu.pk>
2020-09-01 16:11:46 +0000

    refined the code

--- ckanext/ckan_theme/plugin.py
+++ ckanext/ckan_theme/plugin.py
@@ -1,37 +1,40 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import requests
+import json
+import socket
+
+
 def most_popular_datasets():
     '''
     A function to return top 4 datasets based on popularity
 
     '''
-    import requests
-    import json
-    import socket 
-    
-    # Get the ip address   
-    hostname = socket.gethostname()    
-    ip_addr = socket.gethostbyname(hostname)
-    
-    # Make the HTTP request.
-    url = 'http://' + str(ip_addr) + '/api/3/action/package_search?sort=popularity+desc&rows=4'
-    response = requests.get(url)
 
-    assert response.status_code == 200
+    # Get the ip address
+    hostname = socket.gethostname()
+    ip_addr = socket.gethostbyname(hostname)
+
+    # Make the HTTP request.
+    try:
+        url = 'http://' + \
+            str(ip_addr) + '/api/3/action/package_search?sort=popularity+desc&rows=4'
+        response = requests.get(url)
+
+        assert response.status_code == 200
+    except:
+        raise Exception("Sorry, Broken API call")
 
     # Use the json module to load CKAN's response into a dictionary.
     response_dict = response.json()
-    
+
     # Check the contents of the response.
     assert response_dict['success'] is True
     results = response_dict['result']['results']
-    # output = []
-    # for result in results:
-    #     output.append(result['name'])
-            
-    # return output
+
     return results
+
 
 class Ckan_ThemePlugin(plugins.SingletonPlugin):
     # IConfigurer
@@ -44,7 +47,7 @@ class Ckan_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ckan_theme')
-    
+
     def get_helpers(self):
         '''Register the most_popular_datasets() function above as a template
         helper function.
